176a45696d255b6f9a6698bc3fa9e5203f3e8bc8
Robel Fekadu <robelfekadu@gmail.com>
2023-02-01 12:33:09 +0300

    Added docker-compose and dockerfile for orders app as well as docker ignore for the whole monorepo

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -3,7 +3,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Yup from 'yup';
 import { OrdersController } from './orders.controller';
+import { OrdersRepository } from './orders.repository';
 import { OrdersService } from './orders.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/orders.schema';
 
 @Module({
   imports: [
@@ -16,8 +19,9 @@ import { OrdersService } from './orders.service';
       envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
